15675fe587ed19464a1c7b541a64069e98ee7faa
minorblocker <minorblocker@gmail.com>
2023-03-21 14:06:14 +0530

    change assets directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	outDir: './build'
+	outDir: 'build',
+	build: {
+		assets: 'public'
+	}
 });
